a9f39ca9ba560bc8aae99b6cb0ad59ac8d6200b7
Taha Adıgüzel <102309946+Taha-Adiguzel@users.noreply.github.com>
2024-01-01 14:44:38 +0300

    Backend kısmında eklenmeler yapıldı

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
+
 const productRouter = require('./routes/products');
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
+const orderRouter = require('./routes/order');
+const cartRouter = require('./routes/cart');
+
 const port = 3000;
 
 dotenv.config()
@@ -13,6 +19,11 @@ app.use(express.json({limit:'10mb'}));
 app.use(express.urlencoded({limit:'10mb',extended: true}));
 
 app.use('/api/products/',productRouter)
+app.use('/api/',authRouter)
+app.use('/api/users/',userRouter)
+app.use('/api/orders/',orderRouter)
+app.use('/api/carts/',cartRouter)
+
 
 //app.get('/', (req, res) => res.send('Furnite World')) //tarayıcıda localhost:3000 'e giridğimiz zaman ekranda bu çıktı alıyoruz
 app.listen(process.env.PORT || port ,() => console.log(`Example app listening on port ${process.env.PORT}!`)) //terminalde ekrana hangi portta çalıştığı ekrana yazdırılıyor
