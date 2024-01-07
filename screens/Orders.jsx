af8b944b26cffc70863667475447a99c1bb9efc2
Taha-Adiguzel <taha.adiguzel@hotmail.com>
2024-01-07 16:39:19 +0300

    Orders GoBack addet

--- screens/Orders.jsx
+++ screens/Orders.jsx
@@ -1,13 +1,26 @@
-import { StyleSheet ,View} from "react-native";
+import { StyleSheet ,View,SafeAreaView,Text} from "react-native";
+import {Ionicons,SimpleLineIcons} from "@expo/vector-icons";
+import { COLORS } from "../constants";
 import React from 'react';
+import styles from "./cart.style";
 
-const Orders=()=>{
+const Orders=({navigation})=>{
     return(
-        <View>
+        <SafeAreaView style={styles.container}>
+        <View style={styles.titleRow}>
+
+            <Ionicons
+                onPress={()=> navigation.goBack()}
+                name='chevron-back-circle'
+                size ={30}
+                color ={COLORS.primary}
+            />
+          
+            <Text style={styles.titleText}>Orders</Text>
 
         </View>
+        </SafeAreaView>
     )
 }
 
 export default Orders
-const styles=StyleSheet.create({})
